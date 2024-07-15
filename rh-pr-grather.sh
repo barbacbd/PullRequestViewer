d8751d080793fe369829b80cdc3bcb4f2c4f15d9
Brent Barbachem <barbacbd@gmail.com>
2024-07-15 14:51:44 -0400

    Added a script to help with env setup and execution of the software.

---
+++ rh-pr-grather.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# MIT License
+#
+# Copyright (c) 2024 Brent Barbachem
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+VenvPath="/home/$USER/rh-gather-venv"
+PyVersion=`python3 --version | awk '{print $2}'`
+MinPythonVersion="3.9.0"
+
+if [ "$(printf '%s\n' "$MinPythonVersion" "$PyVersion" | sort -V | head -n1)" = "$MinPythonVersion" ]; then 
+    echo "Current python version ${PyVersion} is greater than or equal to ${MinPythonVersion}"
+else
+    echo "ERROR: Python ${PyVersion} is less than minimum python version ${MinPythonVersion}"
+fi
+
+if [ ! -d "$VenvPath" ]; then
+    python3 -m venv $VenvPath
+fi
+
+source $VenvPath/bin/activate
+
+# cover instances where the package may already be installed
+pip install rh_pr_gather --upgrade
+
+# if the output file currently exists then it will be deleted here.
+if [ -f "github.xlsx" ]; then
+    echo "WARN: github.xlsx exists, the file is going to be removed."
+    rm github.xlsx
+fi
+
+# if a a configuration file is found, skip running the configuration step to
+# generate a new configuration file. Warn when this is happens so that the user
+# knows why this was skipped.
+if [ -f "config.yaml" ]; then
+    echo "WARN: config.yaml found. Skipping the config step for rh-pr-gather"
+    rh-pr-gather --input config.yaml
+else
+    rh-pr-gather --config
+fi
+
+deactivate
